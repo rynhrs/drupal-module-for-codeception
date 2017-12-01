269ec591cce09d75f3c54d825e05a9e7c22e752a
Ryan Harris <rynhrs@gmail.com>
2017-12-01 15:11:44 +0100

    Adding auto-login

--- src/Drupal8.php
+++ src/Drupal8.php
@@ -2,14 +2,35 @@
 
 namespace Codeception\Module;
 
+use Drupal\Core\DrupalKernel;
+use Symfony\Component\HttpFoundation\Request;
 use Codeception\Module;
 
 class Drupal8 extends Module
 {
 
-    public function greet($name)
+	private $kernel;
+	private $entityManager;
+
+	public function _initialize()
     {
-        $this->debug("Hello {$name}!");
+
+        $autoloader = require_once 'autoload.php';
+        $request = Request::createFromGlobals();
+        $this->kernel = DrupalKernel::createFromRequest($request, $autoloader, 'prod');
+
+    	$this->kernel->boot();
+
+    	$this->entityManager = $this->kernel->getContainer()->get('entity.manager');
+
+    }
+
+    public function login($username)
+    {
+    	//$user = $this->entityManager->getStorage('user')->load(1);
+		$user = user_load_by_name($username);
+		//print_r($user);
+		user_login_finalize($user);
     }
 
 }
